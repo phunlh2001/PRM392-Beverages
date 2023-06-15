080032caa1891273a1ad35bad2b3ae0d1e1efbab
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-06-16 05:14:02 +0700

    feat: add relation tables

--- app/src/main/java/com/phunlh2001/prm392_beverages/data/entities/relationship/ProductWithOrdersRef.java
+++ app/src/main/java/com/phunlh2001/prm392_beverages/data/entities/relationship/ProductWithOrdersRef.java
@@ -1,2 +1,25 @@
-package com.phunlh2001.prm392_beverages.data.entities.relationship;public class ProductWithOrdersRef {
+package com.phunlh2001.prm392_beverages.data.entities.relationship;
+
+import androidx.room.Embedded;
+import androidx.room.Junction;
+import androidx.room.Relation;
+
+import com.phunlh2001.prm392_beverages.data.entities.Order;
+import com.phunlh2001.prm392_beverages.data.entities.OrderDetail;
+import com.phunlh2001.prm392_beverages.data.entities.Product;
+
+import java.util.List;
+
+public class ProductWithOrdersRef {
+    @Embedded
+    public Product product;
+
+    @Relation(
+            parentColumn = "id", //Product @id
+            entityColumn = "id", //Order @id
+            associateBy = @Junction(value = OrderDetail.class,
+                    parentColumn = "product_id",
+                    entityColumn = "order_id")
+    )
+    public List<Order> orders;
 }
--- app/src/main/java/com/phunlh2001/prm392_beverages/data/entities/relationship/UserEmbedded.java
+++ app/src/main/java/com/phunlh2001/prm392_beverages/data/entities/relationship/UserEmbedded.java
@@ -1,2 +1,20 @@
-package com.phunlh2001.prm392_beverages.data.entities.relationship;public class UserEmebedded {
+package com.phunlh2001.prm392_beverages.data.entities.relationship;
+
+import androidx.room.Embedded;
+import androidx.room.Relation;
+
+import com.phunlh2001.prm392_beverages.data.entities.Order;
+import com.phunlh2001.prm392_beverages.data.entities.User;
+
+import java.util.List;
+
+public class UserEmbedded {
+    @Embedded
+    public User user;
+
+    @Relation(
+            parentColumn = "id",
+            entityColumn = "user_id"
+    )
+    public List<Order> orders;
 }
