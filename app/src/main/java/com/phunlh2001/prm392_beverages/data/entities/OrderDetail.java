dab3503d07680b9a4d98ace89799e1ac76275f5d
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-06-16 05:15:52 +0700

    feat: add weak table and DateConvert type for Datetime

--- app/src/main/java/com/phunlh2001/prm392_beverages/data/entities/OrderDetail.java
+++ app/src/main/java/com/phunlh2001/prm392_beverages/data/entities/OrderDetail.java
@@ -1,2 +1,20 @@
-package com.phunlh2001.prm392_beverages.data.entities;public class OrderDetail {
+package com.phunlh2001.prm392_beverages.data.entities;
+
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.Index;
+
+@Entity(tableName = "OrderDetails",
+        primaryKeys = {"product_id", "order_id"},
+        foreignKeys = {
+            @ForeignKey(entity = Product.class, parentColumns = "id", childColumns = "product_id", onDelete = ForeignKey.CASCADE),
+            @ForeignKey(entity = Order.class, parentColumns = "id", childColumns = "order_id", onDelete = ForeignKey.CASCADE)
+        },
+        indices = {
+                @Index(value = "product_id"),
+                @Index(value = "order_id")
+        })
+public class OrderDetail {
+    public int product_id;
+    public int order_id;
 }
--- app/src/main/java/com/phunlh2001/prm392_beverages/utils/DateConverter.java
+++ app/src/main/java/com/phunlh2001/prm392_beverages/utils/DateConverter.java
@@ -1,2 +1,29 @@
-package com.phunlh2001.prm392_beverages.utils;public class DateConverter {
+package com.phunlh2001.prm392_beverages.utils;
+
+import androidx.room.TypeConverter;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateConverter {
+    @TypeConverter
+    public static String dateToTimestamp(Date date) {
+        SimpleDateFormat formats = new SimpleDateFormat("yyyy-MM-dd");
+        return date == null ? null : formats.format(date);
+    }
+
+    @TypeConverter
+    public static Date fromTimestamp(String value) {
+        if (value == null) return new Date();
+        Date date = new Date();
+        SimpleDateFormat formats = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            date = formats.parse(value);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return date;
+    }
+
 }
