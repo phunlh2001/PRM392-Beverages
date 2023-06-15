19143c740d1209f996bd133af9040b1086d94337
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-06-16 05:10:44 +0700

    fix: delete class Account

---
+++ app/src/main/java/com/phunlh2001/prm392_beverages/data/entities/relationship/UserEmbedded.java
@@ -0,0 +1,2 @@
+package com.phunlh2001.prm392_beverages.data.entities.relationship;public class UserEmebedded {
+}
---
+++ app/src/main/java/com/phunlh2001/prm392_beverages/utils/DateConverter.java
@@ -0,0 +1,2 @@
+package com.phunlh2001.prm392_beverages.utils;public class DateConverter {
+}
