e1cb95323dc60448abeea205dd9b3908ac999c43
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-06-29 16:51:41 +0700

    feat: add Address Selector layout

---
+++ app/src/main/java/com/phunlh2001/prm392_beverages/AddressSelectorActivity.java
@@ -0,0 +1,14 @@
+package com.phunlh2001.prm392_beverages;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddressSelectorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_address_selector);
+    }
+}
