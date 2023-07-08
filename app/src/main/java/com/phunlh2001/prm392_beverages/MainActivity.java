2d4d29a149840a2bab565a954bd1c825973f61a8
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-07-08 23:19:12 +0700

    fix: update database from 2 to 3

--- app/src/main/java/com/phunlh2001/prm392_beverages/MainActivity.java
+++ app/src/main/java/com/phunlh2001/prm392_beverages/MainActivity.java
@@ -3,12 +3,27 @@ package com.phunlh2001.prm392_beverages;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.phunlh2001.prm392_beverages.data.AppDatabase;
+import com.phunlh2001.prm392_beverages.data.dao.ProductDao;
+import com.phunlh2001.prm392_beverages.data.entities.Product;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvMain;
+
+    private void init() {
+        tvMain = findViewById(R.id.tvMain);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+
+//        tvMain.setText(AppDatabase.getInstance(this).productDao().getById(1).getTitle());
     }
 }
