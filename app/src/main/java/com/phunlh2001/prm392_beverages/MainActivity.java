2100090fbe1fab119b297e40edd43318f39a1ec5
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-07-09 20:02:59 +0700

    fix: update db v2 to v3

--- app/src/main/java/com/phunlh2001/prm392_beverages/MainActivity.java
+++ app/src/main/java/com/phunlh2001/prm392_beverages/MainActivity.java
@@ -3,27 +3,12 @@ package com.phunlh2001.prm392_beverages;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.TextView;
-
-import com.phunlh2001.prm392_beverages.data.AppDatabase;
-import com.phunlh2001.prm392_beverages.data.dao.ProductDao;
 
 public class MainActivity extends AppCompatActivity {
-
-    TextView tvMain;
-    ProductDao dao;
-
-    private void init() {
-        tvMain = findViewById(R.id.tvMain);
-        dao = AppDatabase.getInstance(MainActivity.this).productDao();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        init();
-
-        tvMain.setText(dao.getById(1).getTitle());
     }
 }
