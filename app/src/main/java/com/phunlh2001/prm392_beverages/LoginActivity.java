f2446a97d1bba66aaeb56d4478964270ce17b479
Yazt <93716592+KitsuYazt@users.noreply.github.com>
2023-07-11 14:47:32 +0700

    fix

--- app/src/main/java/com/phunlh2001/prm392_beverages/LoginActivity.java
+++ app/src/main/java/com/phunlh2001/prm392_beverages/LoginActivity.java
@@ -1,4 +1,5 @@
 package com.phunlh2001.prm392_beverages;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -6,38 +7,52 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.phunlh2001.prm392_beverages.data.dao.UserDao;
-import com.phunlh2001.prm392_beverages.data.entities.User;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.phunlh2001.prm392_beverages.data.AppDatabase;
+import com.phunlh2001.prm392_beverages.data.dao.UserDao;
+import com.phunlh2001.prm392_beverages.data.entities.User;
 
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.phunlh2001.prm392_beverages.data.AppDatabase;
+import com.phunlh2001.prm392_beverages.data.dao.UserDao;
+import com.phunlh2001.prm392_beverages.data.entities.User;
+
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText usernameEditText, passwordEditText;
-    private Button loginButton, createAccountButton; // Add createAccountButton
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
+    private TextView forgotPasswordTextView;
+    private Button createAccountButton;
 
     private UserDao userDao;
-    private ExecutorService executorService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-        // Initialize database and DAO
-        AppDatabase appDatabase = AppDatabase.getInstance(getApplicationContext());
-        userDao = appDatabase.userDao();
-        executorService = Executors.newSingleThreadExecutor();
+        // Initialize the UserDao
+        userDao = AppDatabase.getInstance(this).userDao();
 
-        // Initialize views
-        usernameEditText = findViewById(R.id.editTextText);
-        passwordEditText = findViewById(R.id.editTextText2);
-        loginButton = findViewById(R.id.button2);
-        createAccountButton = findViewById(R.id.Or_Create); // Initialize createAccountButton
+        // Find views
+        emailEditText = findViewById(R.id.edt_email);
+        passwordEditText = findViewById(R.id.edt_password);
+        loginButton = findViewById(R.id.btn_Login);
+        forgotPasswordTextView = findViewById(R.id.textView5);
+        createAccountButton = findViewById(R.id.Or_Create);
 
         // Set click listener for login button
         loginButton.setOnClickListener(new View.OnClickListener() {
@@ -51,13 +66,9 @@ public class LoginActivity extends AppCompatActivity {
         createAccountButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                navigateToRegister(); // Call navigateToRegister() method
+                navigateToRegister();
             }
         });
-    }
-
-    private void loginUser() {
-        // ... Remaining code for loginUser() method
     }
 
     private void navigateToRegister() {
@@ -65,10 +76,31 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        // Shutdown ExecutorService when the activity is destroyed
-        executorService.shutdown();
+    private void loginUser() {
+        // Get input values
+        String email = emailEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
+
+        // Validate input values (you can add your own validation logic here)
+
+        // Get all users from the database
+        List<User> userList = userDao.getAll();
+
+        // Check if any user matches the given email and password
+        User matchedUser = null;
+        for (User user : userList) {
+            if (user.getEmail().equals(email) && user.getPassword().equals(password)) {
+                matchedUser = user;
+                break;
+            }
+        }
+
+        if (matchedUser != null) {
+            // User exists, proceed with login logic
+            // Show a success message or navigate to the next screen
+        } else {
+            // User doesn't exist or invalid credentials
+            Toast.makeText(LoginActivity.this, "Invalid email or password", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/phunlh2001/prm392_beverages/RegisterActivity.java
+++ app/src/main/java/com/phunlh2001/prm392_beverages/RegisterActivity.java
@@ -1,51 +1,45 @@
 package com.phunlh2001.prm392_beverages;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.phunlh2001.prm392_beverages.data.AppDatabase;
 import com.phunlh2001.prm392_beverages.data.dao.UserDao;
 import com.phunlh2001.prm392_beverages.data.entities.User;
 import com.phunlh2001.prm392_beverages.data.entities.enums.RoleAccount;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.phunlh2001.prm392_beverages.data.AppDatabase;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 public class RegisterActivity extends AppCompatActivity {
 
-    private EditText emailEditText, usernameEditText, passwordEditText, fullNameEditText;
-    private Button registerButton;
+    private EditText emailEditText;
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private EditText fullNameEditText;
+    private Button signUpButton;
 
     private UserDao userDao;
-    private ExecutorService executorService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
 
-        // Initialize database and DAO
-        AppDatabase appDatabase = AppDatabase.getInstance(getApplicationContext());
-        userDao = appDatabase.userDao();
-        executorService = Executors.newSingleThreadExecutor();
+        // Initialize the UserDao
+        userDao = AppDatabase.getInstance(this).userDao();
 
-        // Initialize views
-        emailEditText = findViewById(R.id.editTextTextEmailAddress);
-        usernameEditText = findViewById(R.id.editTextTextUsername);
-        passwordEditText = findViewById(R.id.editTextTextPassword);
+        // Find views
+        emailEditText = findViewById(R.id.Email);
+        usernameEditText = findViewById(R.id.username);
+        passwordEditText = findViewById(R.id.Password);
         fullNameEditText = findViewById(R.id.edit_fullname);
-        registerButton = findViewById(R.id.buttonRegister);
+        signUpButton = findViewById(R.id.buttonRegister);
 
-        // Set click listener for register button
-        registerButton.setOnClickListener(new View.OnClickListener() {
+        // Set click listener for sign up button
+        signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 registerUser();
@@ -54,68 +48,46 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private void registerUser() {
-        // Get user input
+        // Get input values
         String email = emailEditText.getText().toString().trim();
         String username = usernameEditText.getText().toString().trim();
         String password = passwordEditText.getText().toString().trim();
         String fullName = fullNameEditText.getText().toString().trim();
 
-        // Validate user input
-        if (email.isEmpty() || username.isEmpty() || password.isEmpty() || fullName.isEmpty()) {
-            Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
+        // Validate email
+        if (!isValidEmail(email)) {
+            Toast.makeText(RegisterActivity.this, "Invalid email address", Toast.LENGTH_SHORT).show();
             return;
         }
 
-        // Validate email format
-        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            Toast.makeText(this, "Invalid email format. Please enter a valid email address", Toast.LENGTH_SHORT).show();
+        // Validate name
+        if (!isValidName(fullName)) {
+            Toast.makeText(RegisterActivity.this, "Invalid name (maximum 20 words)", Toast.LENGTH_SHORT).show();
             return;
         }
 
-        // Validate full name (only allow letters)
-        if (!fullName.matches("[a-zA-Z ]+")) {
-            Toast.makeText(this, "Invalid full name. Please enter letters only", Toast.LENGTH_SHORT).show();
-            return;
-        }
+        // Validate phone number
 
-        // Validate username (only allow letters)
-        if (!username.matches("[a-zA-Z]+")) {
-            Toast.makeText(this, "Invalid username. Please enter letters only", Toast.LENGTH_SHORT).show();
-            return;
-        }
 
-        // Validate password length
-        if (password.length() > 15) {
-            Toast.makeText(this, "Password must not exceed 15 characters", Toast.LENGTH_SHORT).show();
-            return;
-        }
+        // Create a new User object
+        User user = new User(email, username, password, fullName, "blank_avatar.jpg", RoleAccount.USER);
 
-        // Create a new user object
-        User user = new User(email, username, password, fullName, null, RoleAccount.USER);
+        // Insert the user into the database using UserDao
+        userDao.insert(user);
 
-        // Insert the user into the database using ExecutorService
-        executorService.execute(new Runnable() {
-            @Override
-            public void run() {
-                userDao.insert(user);
-            }
-        });
-
-        // Display success message
-        Toast.makeText(this, "User registered successfully", Toast.LENGTH_SHORT).show();
-
-        // Start LoginActivity
-        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-        startActivity(intent);
-
-        // Finish the activity
-        finish();
+        // Show a success message
+        Toast.makeText(RegisterActivity.this, "Account created successfully", Toast.LENGTH_SHORT).show();
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        // Shutdown ExecutorService when the activity is destroyed
-        executorService.shutdown();
+    private boolean isValidEmail(String email) {
+        return email.contains("@gmail.com");
+    }
+
+    private boolean isValidName(String name) {
+        return name.split(" ").length <= 20;
+    }
+
+    private boolean isValidPhone(String phone) {
+        return phone.matches("\\d+");
     }
 }
