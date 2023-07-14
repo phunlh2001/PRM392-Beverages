ef34c5ac61cba0e16530a9a0972245005f1fa74b
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-07-15 00:41:15 +0700

    feat: add md5

--- app/src/main/java/com/phunlh2001/prm392_beverages/utils/Hash.java
+++ app/src/main/java/com/phunlh2001/prm392_beverages/utils/Hash.java
@@ -1,5 +1,30 @@
 package com.phunlh2001.prm392_beverages.utils;
 
-public class MD5 {
-    public
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Hash {
+    public static String Md5(String str) {
+        final String MD5 = "MD5";
+
+        try {
+            MessageDigest digest = MessageDigest.getInstance(MD5);
+            digest.update(str.getBytes());
+            byte[] messageDigest = digest.digest();
+
+            // Create Hex String
+            StringBuilder hexString = new StringBuilder();
+            for (byte aDigest: messageDigest) {
+                String hex = Integer.toHexString(0xFF & aDigest);
+                while (hex.length() < 2) {
+                    hex = "0" + hex;
+                }
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.fillInStackTrace();
+        }
+        return "";
+    }
 }
