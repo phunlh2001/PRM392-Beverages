f257880e796b5ea976f3fb8842916ee9eccd59e8
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-07-19 07:56:07 +0700

    feat: delivery/store menu

--- app/src/main/java/com/phunlh2001/prm392_beverages/DeliveryMenuActivity.java
+++ app/src/main/java/com/phunlh2001/prm392_beverages/DeliveryMenuActivity.java
@@ -1,17 +1,25 @@
 package com.phunlh2001.prm392_beverages;
 
+import static com.phunlh2001.prm392_beverages.LoginActivity.KEY_LOGIN;
+import static com.phunlh2001.prm392_beverages.LoginActivity.PREF_LOGIN;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.phunlh2001.prm392_beverages.adapters.CategoryAdapter;
 import com.phunlh2001.prm392_beverages.adapters.ProductMenuAdapter;
 import com.phunlh2001.prm392_beverages.data.AppDatabase;
 import com.phunlh2001.prm392_beverages.data.dao.ProductDao;
+import com.phunlh2001.prm392_beverages.data.dao.UserDao;
 import com.phunlh2001.prm392_beverages.data.entities.Product;
+import com.phunlh2001.prm392_beverages.data.entities.User;
 import com.phunlh2001.prm392_beverages.viewmodel.CategoryViewModel;
 
 import java.util.ArrayList;
@@ -19,17 +27,28 @@ import java.util.List;
 
 public class DeliveryMenuActivity extends AppCompatActivity {
 
+    private TextView tvAddress, tvName, tvPhone;
+
     private RecyclerView rcvMenu, rcvCate;
     private ProductMenuAdapter productMenuAdapter;
     private CategoryAdapter categoryAdapter;
+
     private ProductDao productDao;
+    private UserDao userDao;
+
+    private SharedPreferences pref;
 
     private void initialize() {
+        tvAddress = findViewById(R.id.deliveryTo_address);
+        tvName = findViewById(R.id.deliveryTo_name);
+        tvPhone = findViewById(R.id.deliveryTo_numberPhone);
         rcvMenu = findViewById(R.id.rcv_menu);
         rcvCate = findViewById(R.id.rcv_cates);
         productMenuAdapter = new ProductMenuAdapter(this);
         categoryAdapter = new CategoryAdapter(this);
         productDao = AppDatabase.getInstance(this).productDao();
+        userDao = AppDatabase.getInstance(this).userDao();
+        pref = getSharedPreferences(PREF_LOGIN, Context.MODE_PRIVATE);
     }
 
     @Override
@@ -37,6 +56,12 @@ public class DeliveryMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delivery_menu);
         initialize();
+
+        String email = pref.getString(KEY_LOGIN, "");
+        User user = userDao.getUserByEmail(email);
+        tvAddress.setText(user.getAddress());
+        tvName.setText(user.getFull_name());
+        tvPhone.setText(user.getPhone_number());
 
         recyclerViewCategory();
         recyclerViewProduct();
