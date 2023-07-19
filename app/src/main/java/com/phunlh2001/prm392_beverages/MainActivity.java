4f8f6c331a4d6f5d9fc6673bf1a8926d1f74662f
iamonezi <ntquangduy2002@gmail.com>
2023-07-19 19:40:19 +0700

    .

--- app/src/main/java/com/phunlh2001/prm392_beverages/MainActivity.java
+++ app/src/main/java/com/phunlh2001/prm392_beverages/MainActivity.java
@@ -5,10 +5,11 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.search_product);
+        setContentView(R.layout.activity_main);
+
+
     }
 }
---
+++ app/src/main/java/com/phunlh2001/prm392_beverages/SearchProductActivity.java
@@ -0,0 +1,65 @@
+package com.phunlh2001.prm392_beverages;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.phunlh2001.prm392_beverages.adapters.ProductAdapter;
+import com.phunlh2001.prm392_beverages.data.AppDatabase;
+import com.phunlh2001.prm392_beverages.data.dao.ProductDao;
+import com.phunlh2001.prm392_beverages.data.entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchProductActivity extends AppCompatActivity {
+
+    private RecyclerView rcvProduct;
+    private ProductAdapter productAdapter;
+    private List<Product> _list;
+    private ProductDao productDao;
+
+    private void init() {
+        // anh xa
+        rcvProduct = findViewById(R.id.rcv_product);
+
+        // khoi tao adapter
+        productAdapter = new ProductAdapter(this);
+
+        // khoi tao dao
+        productDao = AppDatabase.getInstance(this).productDao();
+    }
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_product);
+
+        init();
+        viewProduct();
+
+
+
+
+    }
+
+    private void viewProduct() {
+        // tao rcv
+        GridLayoutManager gridLayoutManager =new GridLayoutManager(this, 3);
+        rcvProduct.setLayoutManager(gridLayoutManager);
+
+        // set data
+        _list = new ArrayList<>();
+        List<Product> _list = productDao.getAll();
+        productAdapter.setData(_list);
+
+        // set adapter cho rcvMenu
+        rcvProduct.setAdapter(productAdapter);
+
+    }
+}
